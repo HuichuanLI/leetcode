61411e92d49791fd79c125b44725553345adf01e
huichuan <lhc14124908@163.com>
2019-09-12 16:03:20 +0800

    add 26

--- 0026-Remove-Duplicates-from-Sorted-Array/leetcode26.py
+++ 0026-Remove-Duplicates-from-Sorted-Array/leetcode26.py
@@ -4,15 +4,41 @@ class Solution:
         i = 0
         while i < len(nums):
             if nums[i] in a:
-                 nums.pop(nums[i])
+                nums.pop(nums[i])
             else:
-                a.add(nums[i])
+                a.add(i)
                 i += 1
+        return len(nums)
+
+
+class Solution2:
+    def removeDuplicates(self, nums) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == nums[i + 1]:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
+
+
+class Solution3:
+    def removeDuplicates(self, nums) -> int:
+        if not nums:
+            return 0
+
+        tmp_num = None
+        for idx in range(len(nums) - 1, -1, -1):
+            if tmp_num != nums[idx]:
+                tmp_num = nums[idx]
+            else:
+                del nums[idx]
+
         return len(nums)
 
 
 if __name__ == "__main__":
     s = Solution()
-    nums = [1,1]
+    nums = [1, 1]
     print(s.removeDuplicates(nums))
     print(nums)
